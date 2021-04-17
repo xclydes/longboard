d29e1b52aada9f4a5f0942c4a5e74b311072c671
Jermaine Rattray <xclydes@gmail.com>
2021-04-17 11:24:01 -0400

    Initial commit

---
+++ src/main/kotlin/com/xclydes/finance/longboard/LongboardApplication.kt
@@ -0,0 +1,11 @@
+package com.xclydes.finance.longboard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LongboardApplication
+
+fun main(args: Array<String>) {
+    runApplication<LongboardApplication>(*args)
+}
---
+++ src/test/kotlin/com/xclydes/finance/longboard/LongboardApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.xclydes.finance.longboard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LongboardApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
